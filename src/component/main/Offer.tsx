29b78243d750a808411d4b14e303e228144b1170
Randolph Mayson Dy <mayson.dy@gmail.com>
2023-11-01 21:51:22 +0800

    Update offer section

--- src/component/main/Offer.tsx
+++ src/component/main/Offer.tsx
@@ -1,5 +1,7 @@
 import GradientText from "../shared/GradientText";
 import RevealAnimation from "../shared/RevealAnimation";
+import animationData from "../../assets/lottie/offer.json";
+import Lottie from "lottie-react";
 
 type CardType = {
   label: string;
@@ -8,45 +10,67 @@ type CardType = {
   darkMode: boolean;
 };
 
-const Card = ({ label, title, description, darkMode }: CardType) => {
+const Offer = () => {
   return (
-    <div className={`my-2 md:my-0 w-full border hover:scale-110 p-6 rounded-lg group relative overflow-hidden ${darkMode ? 'border-white': 'border-dark'}`}>
-      <div className="text-xl md:text-3xl">
-        { label }
+    <div className="p-2">
+      <div className="text-3xl md:text-6xl text-center md:mt-12 my-16">
+        My Services
       </div>
-      <RevealAnimation leftToRight={true}>
-        <GradientText text={title} classes="font-main text-2xl md:text-4xl font-bold py-2 mt-6" />
-      </RevealAnimation>
-      <div className="absolute inset-0 bg-black bg-opacity-90 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-        <div className="text-white text-center absolute inset-0 flex items-center justify-center p-4">
-          {description}
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-12 px-4 md:px-10 md:mb-12">
+        <div>
+          <Lottie animationData={animationData} />
+        </div>
+        <div className="flex flex-col justify-center gap-8 items-center">
+          <div>
+            <RevealAnimation leftToRight={true}>
+              <GradientText
+                text="Angular and React Development"
+                classes="font-main text-xl md:text-3xl font-bold"
+              />
+            </RevealAnimation>
+            <div>
+              Expert frontend development using Angular and React for responsive
+              and bug-free web applications.
+            </div>
+          </div>
+          <div>
+            <RevealAnimation leftToRight={true}>
+              <GradientText
+                text="UI Design and Development"
+                classes="font-main text-xl md:text-3xl font-bold"
+              />
+            </RevealAnimation>
+            <div>
+              Craft visually appealing, responsive and user-friendly interfaces,
+              enhancing user engagement.
+            </div>
+          </div>
+          <div>
+            <RevealAnimation leftToRight={true}>
+              <GradientText
+                text="Custom Frontend Solutions"
+                classes="font-main text-xl md:text-3xl font-bold"
+              />
+            </RevealAnimation>
+            <div>
+              Tailored services for unique web applications, interface
+              optimization, and specialized UI/UX design.
+            </div>
+          </div>
+          <div>
+            <RevealAnimation leftToRight={true}>
+              <GradientText
+                text="Performance Optimization"
+                classes="font-main text-xl md:text-3xl font-bold"
+              />
+            </RevealAnimation>
+            <div>
+              Optimize web app performance, refactor code for efficiency, and
+              enhance load times.
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-const Offer = ({ darkMode }: { darkMode: boolean }) => {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-3 my-6 md:gap-12 px-4 md:px-10 md:mb-12">
-      <Card
-        label=".01"
-        title="Front-end Development"
-        description="Translate designs into interactive websites with HTML, CSS, JavaScript, animations, and cross-browser compatibility."
-        darkMode={darkMode}
-      />
-      <Card
-        label=".02"
-        title="Responsive Web Design"
-        description="Create adaptable, user-friendly websites for diverse devices, enhancing accessibility and engagement."
-        darkMode={darkMode}
-      />
-      <Card
-        label=".03"
-        title="Modern SPA Development"
-        description="Develop high-performance SPAs with Angular/React, delivering real-time updates and exceptional UX."
-        darkMode={darkMode}
-      />
     </div>
   );
 };
